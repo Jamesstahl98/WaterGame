e4177751266a2b6ad36a2ca6e88c5091792c3e17
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-10-11 12:47:20 +0200

    DayNightCycle.cs
    Added script for handling time

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private float startTime = 8f;
+    public float CurrentTime
+    {
+        get { return (Time.time + startTime) % 24f; }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log(CurrentTime);
+        }
+    }
+
+    private void UpdateTime()
+    {
+
+    }
+}
